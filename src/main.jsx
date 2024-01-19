6d7840d904f402b6b1892adf4abced1975917b81
Abdullh1111 <abdullah4474032@gmail.com>
2024-01-19 10:05:11 +0600

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,32 @@ import {
 } from "react-router-dom";
 import Root from './Components/Root/Root';
 import Home from './Components/Home/Home';
+import Jobs from './Components/Jobs/Jobs';
+import Applied from './Components/Applied.jsx/Applied';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+import Blog from './Components/Blogs/Blog';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: '/jobs',
+        element: <Jobs></Jobs>
+      },
+      {
+        path: '/applied',
+        element: <Applied></Applied>
+      },
+      {
+        path: '/blogs',
+        element: <Blog></Blog>
       }
     ]
   },
